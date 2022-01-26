28ebe5cf65a357c5b48d5704685d2a5078411c33
ABC <85392015@qq.com>
2022-01-26 12:28:09 +0800

    words

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,11 +3,11 @@ require("@nomiclabs/hardhat-waffle");
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+	const accounts = await hre.ethers.getSigners();
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
+	for (const account of accounts) {
+		console.log(account.address);
+	}
 });
 
 // You need to export an object to set up your config
@@ -17,10 +17,18 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  defaultNetwork: 'hardhat',
+	defaultNetwork: 'hardhat',
 	networks: {
 		hardhat: {
 
+		},
+		kovan: {
+			url: 'https://kovan.infura.io/v3/c7cd730e3f1e4f9a8c702c6cb9d17f3f',
+			chainId: 42,
+			from: process.env.ETH_ADDRESS_0,
+			accounts: [
+				process.env.ETH_PK_0,
+			]
 		},
 		matic_mainnet: {
 			// url: 'https://rpc-mainnet.maticvigil.com/v1/6ca36da1323f40dc42d64ed9ba89da9a6f59c23d',
@@ -36,5 +44,14 @@ module.exports = {
 			]
 		}
 	},
-  solidity: "0.8.4",
+	solidity: {
+		compilers: [
+			{
+				version: '0.8.4',
+			},
+			{
+				version: '0.6.6',
+			}
+		]
+	}
 };
